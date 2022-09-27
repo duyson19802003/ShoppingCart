523a569e0484c80cc72bf482927217c1845de6db
DuySon <duyson19802003@gmail.com>
2022-09-27 16:00:32 +0700

    Component to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'PRODUCT ITEM NUMBER 1',
+      description: 'Description for product item number 1',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 5.99,
+      quantity: 2,
+    },
+    {
+      id: '2',
+      name: 'PRODUCT ITEM NUMBER 2',
+      description: 'Description for product item number 2',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 9.99,
+      quantity: 1,
+    },
+    {
+      id: '3',
+      name: 'PRODUCT ITEM NUMBER 3',
+      description: 'Description for product item number 3',
+      thumbnail: 'https://via.placeholder.com/200x150',
+      price: 19.99,
+      quantity: 5,
+    },
+  ];
+
+  numberItems: number = 2;
+  subTotal: number = 5;
+
+  removeProduct(productId: string) {
+    // alert('Remove product: ' + productId);
+    const index = this.products.findIndex((p) => p.id === productId);
+    if (index !== -1) this.products.splice(index, 1);
+
+    let numberItems = 0;
+    let subTotal = 0;
+    for (const product of this.products) {
+      numberItems += product.quantity;
+      subTotal += product.price * product.quantity;
+    }
+
+    this.numberItems = numberItems;
+    this.subTotal = subTotal;
+  }
 }
--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,13 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
-class Product {
-  id: string = '';
-  name: string = '';
-  description: string = '';
-  thumbnail: string = '';
-  price: number = 0;
-  quantity: number = 0;
-}
+import { EmitterVisitorContext } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from './../product.model';
 
 @Component({
   selector: 'app-products-list',
@@ -15,39 +8,14 @@ class Product {
   styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'PRODUCT ITEM NUMBER 1',
-      description: 'Description for product item number 1',
-      thumbnail: 'https://via.placeholder.com/200x150',
-      price: 5.99,
-      quantity: 2,
-    },
-    {
-      id: '2',
-      name: 'PRODUCT ITEM NUMBER 2',
-      description: 'Description for product item number 2',
-      thumbnail: 'https://via.placeholder.com/200x150',
-      price: 9.99,
-      quantity: 1,
-    },
-    {
-      id: '3',
-      name: 'PRODUCT ITEM NUMBER 3',
-      description: 'Description for product item number 3',
-      thumbnail: 'https://via.placeholder.com/200x150',
-      price: 19.99,
-      quantity: 5,
-    },
-  ];
+  @Input() products: Product[] = [];
+  @Output() onRemoveProduct = new EventEmitter();
 
   constructor() {}
-
   ngOnInit(): void {}
+
   removeProduct(productId: string): void {
-    const index = this.products.findIndex((p) => p.id === productId);
-    if (index !== -1) this.products.splice(index, 1);
+    this.onRemoveProduct.emit(productId);
   }
   updateQuantity(quantity: any, productId: string): void {
     console.log('The quantity is: ' + quantity.value);
